b003ec3edbde647700b272e871a33905b09f954b
Alexceptiion <alex_rama@hotmail.fr>
2021-05-28 09:53:51 +0200

    maj

--- origo/src/app/app.module.ts
+++ origo/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { HomeComponent } from './home/home.component';
 import { GamesComponent } from './games/games.component';
 import { AccountComponent } from './account/account.component';
@@ -43,6 +45,8 @@ const routes: Routes = [
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatMenuModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
